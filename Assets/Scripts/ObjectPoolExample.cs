48994539d71c00a50de02fd9d2a18658bd177f14
SkyWatcher <herobrinehater101@gmail.com>
2024-02-27 11:28:03 -0600

    Created a temporary example of object pooling

---
+++ Assets/Scripts/ObjectPoolExample.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class ObjectPoolExample : MonoBehaviour
+{
+    private ObjectPool<Transform> _transformPool;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateTransformPool();
+    } 
+    
+    void Update()
+    {
+        Transform transform = _transformPool.Get();
+        
+        _transformPool.Release(transform);
+    }
+
+    void CreateTransformPool()
+    {
+        _transformPool = new ObjectPool<Transform>(() =>
+        {
+            //return Instantiate(GameObject); //Instantiate whatever prefab you're using
+        }, transform =>
+        {
+            transform.GameObject().SetActive(true);
+        }, transform1 =>
+        {
+            transform.GameObject().SetActive(false);
+        }, transform =>
+        {
+            Destroy(transform);
+        }, false, 20, 40);
+    }
+}
